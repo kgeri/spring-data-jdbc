57962f8e2c1ee97a6ccc8229877b628ae7a178ec
Greg L. Turnquist <gturnquist@vmware.com>
2020-11-12 09:06:19 -0600

    Yo

--- ci/rootless-docker.bash
+++ ci/rootless-docker.bash
@@ -15,8 +15,10 @@ if [ ! -d $XDG_RUNTIME_DIR ]; then
     curl -fsSL https://get.docker.com/rootless | sh > init.sh
 fi
 
+ls -l
+
 cat init.sh
-source init.sh
+source <(grep '^export' init.sh)
 PATH=$HOME/bin:$PATH dockerd-rootless.sh --experimental --storage-driver vfs &
 sleep 1
 DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock docker info || ls -la $XDG_RUNTIME_DIR
