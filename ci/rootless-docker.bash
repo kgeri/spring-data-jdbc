cf61b495ef752e14a48cb0ebed69ff2c20acdd39
Greg L. Turnquist <gturnquist@vmware.com>
2020-11-12 08:58:03 -0600

    yo

--- ci/rootless-docker.bash
+++ ci/rootless-docker.bash
@@ -5,7 +5,7 @@ set -euo pipefail
 # Ignore the fact that rootful Docker is running on the agent
 export FORCE_ROOTLESS_INSTALL=1
 
-XDG_RUNTIME_DIR=/tmp/docker-spring-data
+export XDG_RUNTIME_DIR=/tmp/docker-spring-data
 
 if [ ! -d $XDG_RUNTIME_DIR ]; then
     echo "Don't appear to have rootless Docker!"
@@ -13,10 +13,10 @@ if [ ! -d $XDG_RUNTIME_DIR ]; then
     mkdir -p $XDG_RUNTIME_DIR || true
 
     curl -fsSL https://get.docker.com/rootless | sh > init.sh
-    cat init.sh
 fi
 
-source <(grep '^export' init.sh)
+cat init.sh
+source < init.sh
 PATH=$HOME/bin:$PATH dockerd-rootless.sh --experimental --storage-driver vfs &
 sleep 1
 DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock docker info || ls -la $XDG_RUNTIME_DIR
