1b0079f3796d4486ddc33ffda6239fbbf3144941
Greg L. Turnquist <gturnquist@vmware.com>
2020-11-16 11:13:26 -0600

    Yo

--- test.sh
+++ test.sh
@@ -7,11 +7,10 @@ export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
 
 ls -l /var/run/docker.sock
 
-pushd .. && ./accept-third-party-license.sh && popd
+./accept-third-party-license.sh
 
 mkdir -p /tmp/jenkins-home
 
-docker info -f '{{ join .SecurityOptions "\n" }}'
 docker run -it --rm busybox whoami
 
-MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/jenkins-home" ./mvnw -Pci,all-dbs clean dependency:list test -Dsort -U -B
+#MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/jenkins-home" ./mvnw -Pci,all-dbs clean dependency:list test -Dsort -U -B
