dafab8d0a4f860a08ff36f32824e4f3218a02194
Greg L. Turnquist <gturnquist@vmware.com>
2020-11-16 11:21:17 -0600

    Yo

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-export XDG_RUNTIME_DIR=/var/run/docker.sock
+export XDG_RUNTIME_DIR=/var/run
 export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
 
 ls -l /var/run/docker.sock
