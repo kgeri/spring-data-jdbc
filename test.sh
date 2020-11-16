8f58e234fb8d754c969c44e223bda1a5c8880ff9
Greg L. Turnquist <gturnquist@vmware.com>
2020-11-16 11:42:21 -0600

    Yo

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-export XDG_RUNTIME_DIR=/var/run
-export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
+#export XDG_RUNTIME_DIR=/var/run
+#export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
 
 ls -l /var/run/docker.sock
 
@@ -13,6 +13,6 @@ mkdir -p /tmp/jenkins-home
 
 docker run --rm busybox whoami
 
-docker run -v $XDG_RUNTIME_DIR/docker.sock:/var/run/docker.sock --rm testcontainers/ryuk:0.3.0
+docker run -v /var/run/docker.sock:/var/run/docker.sock --rm testcontainers/ryuk:0.3.0
 
 #MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/jenkins-home" ./mvnw -Pci,all-dbs clean dependency:list test -Dsort -U -B
